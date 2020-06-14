55837647d5701221b54ad545d234570050131789
nouh ben kadi <benkadi.nouh@icloud.com>
2020-06-14 18:30:18 +0100

    Finnished the xylophone app

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,53 @@
+import 'package:audioplayers/audio_cache.dart';
 import 'package:flutter/material.dart';
 
 void main() => runApp(XylophoneApp());
 
 class XylophoneApp extends StatelessWidget {
+  void playSound({String note}) {
+    final player = new AudioCache();
+    player.play('$note.wav');
+  }
+
+  Expanded _buildKey({Color color, String noteName}) {
+    return Expanded(
+      child: FlatButton(
+        color: color,
+        onPressed: () {
+          playSound(note: noteName);
+        },
+        child: null,
+      ),
+    );
+  }
+
+  _buildKey2({Color color, String noteName}) => Expanded(
+        child: FlatButton(
+          color: color,
+          onPressed: () {
+            playSound(note: noteName);
+          },
+          child: null,
+        ),
+      );
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
+        backgroundColor: Colors.black,
         body: SafeArea(
-          child: Container(),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              _buildKey(color: Colors.redAccent[400], noteName: 'note1'),
+              _buildKey(color: Colors.blueAccent[400], noteName: 'note2'),
+              _buildKey(color: Colors.yellowAccent[400], noteName: 'note3'),
+              _buildKey(color: Colors.purpleAccent[400], noteName: 'note4'),
+              _buildKey(color: Colors.indigoAccent[400], noteName: 'note5'),
+              _buildKey(color: Colors.lightGreenAccent[400], noteName: 'note6'),
+              _buildKey2(color: Colors.orangeAccent[400], noteName: 'note7'),
+            ],
+          ),
         ),
       ),
     );
